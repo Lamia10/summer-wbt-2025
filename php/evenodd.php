39b29e45d83250990fd80b6b294f6824656ec886
Sanjana Rahman <sanjana.lamia120@gmail.com>
2025-08-27 15:55:47 +0600

    Delete file from repository

--- php/evenodd.php
+++
@@ -1,9 +0,0 @@
-<?php
-$num = 7;   
-
-if($num % 2 == 0){
-    echo "$num is Even";
-} else {
-    echo "$num is Odd";
-}
-?>
--- php/vat.php
+++
@@ -1,8 +0,0 @@
-<?php
-$amount = 1000;   // set
-$vat = ($amount * 15) / 100;
-
-echo "Amount = $amount<br>";
-echo "VAT (15%) = $vat<br>";
-echo "Total with VAT = " . ($amount + $vat) . "<br>";
-?>
